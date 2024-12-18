ef9588116d29cc37b1f61e73577dfb0b2bc6d586
Dimalsha Perera <139336872+DimalshaPerera@users.noreply.github.com>
2024-12-18 12:25:20 +0530

    Completed the sign up page and forgot pw page
    Completed the sign up page and forgot pw page added relevant images and svgs

--- lib/pages/forgotPwSecond.dart
+++ lib/pages/forgotPwSecond.dart
@@ -1,33 +1,109 @@
 import 'package:flutter/material.dart';
+import 'package:fynaura/pages/forgotPwSecond.dart';
 import '../widgets/CustomButton.dart';
-import '../widgets/backBtn.dart'; // Import the renamed custom button
+import '../widgets/backBtn.dart';
+import 'package:fynaura/pages/mainLogin.dart';
+import '../widgets/customInput.dart';// Import the renamed custom button
 
-class forgotPwSecond extends StatefulWidget {
-  const forgotPwSecond({super.key});
+class ForgotPWSecond extends StatefulWidget {
+  const ForgotPWSecond({super.key});
 
   @override
-  State<forgotPwSecond> createState() => ForgotPwSecond();
+  State<ForgotPWSecond> createState() => ForgotPWSecondstate();
 }
 
-class ForgotPwSecond extends State<forgotPwSecond> {
+class ForgotPWSecondstate extends State<ForgotPWSecond> {
   @override
   Widget build(BuildContext context) {
+
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text(''),
-        leading: CustomBackButton(), // Use your custom button here
+        leading: CustomBackButton(), // Custom back button
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              "Welcome to the Login Page!",
-              style: TextStyle(fontSize: 24),
-            ),
-            const SizedBox(height: 20),
-            CustomBackButton(), // Use the custom button here again
-          ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+
+              const Text(
+                "Enter OTP Code",
+                style: TextStyle(
+                  fontFamily: 'Urbanist',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                ),
+              ),
+
+              const SizedBox(height: 10),
+              // Subtext
+              const Text(
+                "Check your email to see the verification code",
+                style: TextStyle(
+                  fontFamily: 'Urbanist',
+                  fontWeight: FontWeight.w600,
+                  fontSize: 14,
+                  color: const Color(0xFF6A707C),
+                ),
+              ),
+
+              const SizedBox(height: 30),
+
+              // Email Input
+              CustomInputField(
+                hintText: "Enter your Email",
+                controller: TextEditingController(),
+              ),
+              const SizedBox(height: 20),
+
+              CustomButton(
+                text: "Send Code",
+                backgroundColor: const Color(0xFF1E232C),
+                textColor: Colors.white,
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const ForgotPWSecond()),
+                  );
+                },
+              ),
+              const SizedBox(height: 500),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Didn't get the Code?",
+                    style: TextStyle(
+                      fontFamily: 'Urbanist',
+                      fontWeight: FontWeight.w600,
+                      fontSize: 14,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {   Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const Mainlogin()),
+                    );},
+                    child: const Text(
+                      "Send Again",
+                      style: TextStyle(
+                        fontFamily: 'Urbanist',
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFF254E7A),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+
+
+
+
+            ],
+          ),
         ),
       ),
     );
--- lib/pages/mainSignUp.dart
+++ lib/pages/mainSignUp.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:fynaura/pages/mainLogin.dart';
 import '../widgets/CustomButton.dart';
-import '../widgets/backBtn.dart'; // Import the renamed custom button
+import '../widgets/backBtn.dart';
+import 'package:fynaura/pages/forgotPwFirst.dart';
+import '../widgets/customInput.dart';
 
 class Mainsignup extends StatefulWidget {
   const Mainsignup({super.key});
@@ -14,20 +17,194 @@ class _MainSignupState extends State<Mainsignup> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(''),
-        leading: CustomBackButton(), // Use your custom button here
+        leading: CustomBackButton(), // Custom back button
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              "Welcome to the Login Page!",
-              style: TextStyle(fontSize: 24),
-            ),
-            const SizedBox(height: 20),
-            CustomBackButton(), // Use the custom button here again
-          ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+
+              const Text(
+                "Register!",
+                style: TextStyle(
+                  fontFamily: 'Urbanist',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                ),
+              ),
+
+
+              // Subtext
+              // const Text(
+              //   "Enter your email and password to login",
+              //   style: TextStyle(
+              //     fontFamily: 'Urbanist',
+              //     fontWeight: FontWeight.w600,
+              //     fontSize: 14,
+              //     color: const Color(0xFF6A707C),
+              //   ),
+              // ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const Text(
+                    "Already have an account?",
+                    style: TextStyle(
+                      fontFamily: 'Urbanist',
+                      fontWeight: FontWeight.w600,
+                      fontSize: 14,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {   Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const Mainlogin()),
+                    );},
+                    child: const Text(
+                      "Login with",
+                      style: TextStyle(
+                        fontFamily: 'Urbanist',
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFF254E7A),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 32),
+              CustomInputField(
+                hintText: "Username",
+                controller: TextEditingController(),
+              ),
+              const SizedBox(height: 20),
+              // Email Input
+              CustomInputField(
+                hintText: "Email",
+                controller: TextEditingController(),
+              ),
+              const SizedBox(height: 20),
+
+              // Forgot Password
+              CustomInputField(
+                hintText: "Password",
+                controller: TextEditingController(),
+                obscureText: true, // This will obscure the text for password input
+              ),
+              const SizedBox(height: 20),
+              CustomInputField(
+                hintText: "Confirm your Password",
+                controller: TextEditingController(),
+                obscureText: true, // This will obscure the text for password input
+              ),
+              // Forgot Password
+         
+              const SizedBox(height: 20),
+
+
+              CustomButton(
+                text: "Register",
+                backgroundColor: const Color(0xFF1E232C),
+                textColor: Colors.white,
+                onPressed: () {
+                  print("Login pressed,open home");
+                },
+              ),
+              const SizedBox(height: 180),
+
+
+              // OR Divider
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(child: Divider(color: Colors.grey.shade300)),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Text(
+                      "Or Register with",
+                      style: TextStyle(
+                        fontFamily: 'Urbanist',
+                        fontWeight: FontWeight.w600,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+                  //creates a horizontal line
+                  Expanded(child: Divider(color: const Color(0xFFE8ECF4))),
+                ],
+              ),
+              const SizedBox(height: 20),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // Google Icon
+                  Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle, // Makes it round
+                      border: Border.all(color: Color(0xFFEFF0F6), width: 2), // Stroke
+                    ),
+                    padding: EdgeInsets.all(10), // Space inside the border
+                    child: Image.asset(
+                      "images/google.png",
+                      height: 24,
+                      width: 24,
+                    ),
+                  ),
+                  const SizedBox(width: 15), // Spacing between icons
+
+                  // Facebook Icon
+                  Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(color: Color(0xFFEFF0F6), width: 2),
+                    ),
+                    padding: EdgeInsets.all(10),
+                    child: Image.asset(
+                      "images/fb.png",
+                      height: 24,
+                      width: 24,
+                    ),
+                  ),
+                  const SizedBox(width: 15),
+
+                  // Apple Icon
+                  Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(color: Color(0xFFEFF0F6), width: 2),
+                    ),
+                    padding: EdgeInsets.all(10),
+                    child: Image.asset(
+                      "images/apple.png",
+                      height: 24,
+                      width: 24,
+                    ),
+                  ),
+                  const SizedBox(width: 15),
+
+                  // Phone Icon
+                  Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(color: Color(0xFFEFF0F6), width: 2),
+                    ),
+                    padding: EdgeInsets.all(10),
+                    child: Image.asset(
+                      "images/cellphone.png",
+                      height: 24,
+                      width: 24,
+                    ),
+                  ),
+                ],
+              ),
+
+
+
+
+            ],
+          ),
         ),
       ),
     );
