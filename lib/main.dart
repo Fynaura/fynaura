24bd20c382b5f33571bce60c3f8bfa5983c55adb
shone26 <153415158+shone26@users.noreply.github.com>
2024-12-18 22:21:07 +0530

    creating a page for home

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,66 @@
 import 'package:flutter/material.dart';
+<<<<<<< Updated upstream
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:fynaura/loginScreen.dart';
+=======
+import 'dart:async';
+>>>>>>> Stashed changes
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+<<<<<<< Updated upstream
     return ScreenUtilInit(
       designSize: Size(402, 874),
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         home: LoginPage(),
       ),
+=======
+    return MaterialApp(
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomePage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset('assets/splash/splash_image.png'),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Home Page')),
+      body: Center(child: Text('Welcome to the Home Page!')),
+>>>>>>> Stashed changes
     );
   }
 }
