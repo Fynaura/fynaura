7e03743e05c36520b422a04b414eb414d3a3b47a
Jayindu <147903908+Jayindu@users.noreply.github.com>
2025-02-11 16:32:56 +0530

    add a screen to show extracted details

---
+++ lib/extracted_text_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class ExtractedTextScreen extends StatelessWidget {
+  final String text;
+
+  const ExtractedTextScreen({super.key, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        foregroundColor: Colors.blue[300],
+        backgroundColor: Colors.grey[200],
+        title: const Text("Extracted Details"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: SingleChildScrollView(
+          child: Text(
+            text,
+            textAlign: TextAlign.left,
+            style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+          ),
+        ),
+      ),
+    );
+  }
+}
