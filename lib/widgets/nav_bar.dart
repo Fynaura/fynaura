239512c5dc799c5285cbe49fcf8586dd005ca72c
shone26 <153415158+shone26@users.noreply.github.com>
2025-02-11 21:11:07 +0530

    Update nav_bar.dart

--- lib/widgets/nav_bar.dart
+++ lib/widgets/nav_bar.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class NavBar extends StatelessWidget{
+class NavBar extends StatelessWidget {
   final int pageIndex;
   final Function(int) onTap;
 
@@ -11,23 +11,21 @@ class NavBar extends StatelessWidget{
     super.key,
     required this.pageIndex,
     required this.onTap,
-
   });
-
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.only(
-        left: 10,
-        right: 10,
-        bottom: Platform.isAndroid ? 16:0,
+        left: 0,
+        right: 0,
+        bottom: 0,
       ),
       child: BottomAppBar(
         elevation: 0.0,
+        color: Color(0xFF85C1E5), // Set the BottomAppBar to blue
         child: Container(
           height: 60,
-          color: Color(0xFF85C1E5),
           child: Row(
             children: [
               navItem(
@@ -35,25 +33,22 @@ class NavBar extends StatelessWidget{
                 pageIndex == 0,
                 onTap: () => onTap(0),
                 label: 'Home',
-
               ),
               navItem(
                 CupertinoIcons.search,
                 pageIndex == 1,
                 onTap: () => onTap(1),
                 label: 'Search',
-
               ),
               navItem(
                 CupertinoIcons.calendar_today,
                 pageIndex == 2,
                 onTap: () => onTap(2),
                 label: 'Plan',
-
               ),
               navItem(
                 CupertinoIcons.person,
-                pageIndex ==3,
+                pageIndex == 3,
                 onTap: () => onTap(3),
                 label: 'Profile',
               ),
@@ -66,13 +61,13 @@ class NavBar extends StatelessWidget{
 
   Widget navItem(IconData icon, bool selected, {Function()? onTap, required String label}) {
     return Expanded(
-        child: InkWell(
-          onTap:onTap,
-          child: Icon(
-            icon,
-            color: selected ? Colors.white : Colors.white.withOpacity(0.4),
-          ),
+      child: InkWell(
+        onTap: onTap,
+        child: Icon(
+          icon,
+          color: selected ? Colors.white : Colors.white.withOpacity(0.4),
         ),
+      ),
     );
   }
 }
