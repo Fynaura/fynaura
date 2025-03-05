d9852715935effaec0090a7765c39ca25d7bd326
shone26 <153415158+shone26@users.noreply.github.com>
2025-03-05 15:46:54 +0530

    refactor the backend part to typescript

---
+++ back-end/src/transactions/transactions.controller.ts
@@ -0,0 +1,22 @@
+/* eslint-disable prettier/prettier */
+
+import { Controller } from '@nestjs/common';
+
+@Controller('transactions')
+export class TransactionsController {
+    // constructor(private readonly transactionsService: TransactionService) {
+
+    // }
+
+    // @Public()
+    // @Get()
+    // findAll(): Promise<any> {
+    //     return this.transactionsService.findAll();
+    // }
+    // @Public()
+    // @Get('users')
+    // getHello(): Promise<any> {
+    //     return clerkClient.users.getUserList();
+    // }
+}
+
