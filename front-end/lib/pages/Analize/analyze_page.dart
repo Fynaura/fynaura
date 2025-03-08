be85baded1c1410d3542721a2b1b10a8f3a10b33
DivyanNimsara <divyannim2003@gmail.com>
2025-03-08 14:43:13 +0530

    Update analyze_page.dart

--- front-end/lib/pages/Analize/analyze_page.dart
+++ front-end/lib/pages/Analize/analyze_page.dart
@@ -28,3 +28,47 @@ class _AnalyzePageState extends State<AnalyzePage> {
     );
   }
 }
+// Inside Column in Scaffold body
+
+Padding(
+  padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 10),
+  child: Container(
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(10),
+      color: const Color.fromARGB(255, 221, 221, 221),
+    ),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 3),
+          child: Container(
+            width: MediaQuery.of(context).size.width / 4,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.white,
+            ),
+            padding: EdgeInsets.symmetric(vertical: 7),
+            child: Center(
+                child: Text(
+              "Today",
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+            )),
+          ),
+        ),
+        // Other buttons (This Week, This Month)
+      ],
+    ),
+  ),
+),
+Padding(
+  padding: const EdgeInsets.symmetric(horizontal: 15.0),
+  child: Text(
+    "Recent Transactions",
+    style: TextStyle(
+      fontWeight: FontWeight.bold,
+      fontSize: 18,
+    ),
+  ),
+),
+// ListView.builder for transactions
