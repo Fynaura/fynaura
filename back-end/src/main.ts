c4f20b58fbd6e8f36793c056b1f423212e085200
shone26 <153415158+shone26@users.noreply.github.com>
2025-03-15 16:59:55 +0530

    Feat - added the firebase authentication

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,12 +1,41 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable prettier/prettier */
 
 
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as firebaseAdmin from 'firebase-admin';
+import * as fs from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+  .setTitle('User Authentication')
+  .setDescription(
+    'The API details for the User Authentication Demo application using Firebase in the NestJS backend.',
+  )
+  .setVersion('1.0')
+  .addTag('Authentication')
+  .addBearerAuth()
+  .build();
+
+  //firebase ;
+  const firebaseKeyFilePath =
+    'C:/IIT Notes/L5 - Degree/Software Development Group Project/Actice/New folder (2)/fynaura/back-end/src/authentication/firebase_auth.json';
+  const firebaseServiceAccount /*: ServiceAccount*/ = JSON.parse(
+    fs.readFileSync(firebaseKeyFilePath).toString(),
+  );
+  if (firebaseAdmin.apps.length === 0) {
+    console.log('Initialize Firebase Application.');
+    firebaseAdmin.initializeApp({
+      credential: firebaseAdmin.credential.cert(firebaseServiceAccount),
+    });
+  }
+
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
