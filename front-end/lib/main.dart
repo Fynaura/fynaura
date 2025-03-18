46434a8149b058d04ae10fc642db56ed8fa2da85
Dimalsha Perera <139336872+DimalshaPerera@users.noreply.github.com>
2025-03-18 12:00:37 +0530

    SCRUM -011-CI-Fixed issues in main

--- front-end/lib/main.dart
+++ front-end/lib/main.dart
@@ -1,3 +1,56 @@
+// import 'package:flutter/material.dart';
+// import 'package:fynaura/pages/Analize/analyze_page.dart';
+// import 'dart:async';
+// import 'package:fynaura/pages/log-in/mainLogin.dart';
+// import 'package:fynaura/pages/sign-up/mainSignUp.dart';
+// import 'package:permission_handler/permission_handler.dart';
+//
+// void main() {
+//   runApp(MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: SplashScreen(),
+//     );
+//   }
+// }
+//
+// class SplashScreen extends StatefulWidget {
+//   @override
+//   _SplashScreenState createState() => _SplashScreenState();
+// }
+//
+// class _SplashScreenState extends State<SplashScreen> {
+//   @override
+//   void initState() {
+//     super.initState();
+//     Timer(Duration(seconds: 5), () {
+//       Navigator.pushReplacement(
+//         context,
+//         // MaterialPageRoute(builder: (context) => AnalyzePage()),
+//         MaterialPageRoute(builder: (context) => Mainsignup()),
+//       );
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Color(0xFF85C1E5), // Set the background color
+//       body: Center(
+//         child: Image.asset('images/fynaura.png'),
+//       ),
+//     );
+//   }
+// }
+// void requestAlarmPermission() async {
+//   if (await Permission.scheduleExactAlarm.isDenied) {
+//     await Permission.scheduleExactAlarm.request();
+//   }
+// }
 import 'package:flutter/material.dart';
 import 'package:fynaura/pages/Analize/analyze_page.dart';
 import 'dart:async';
@@ -6,14 +59,16 @@ import 'package:fynaura/pages/sign-up/mainSignUp.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());  // Ensure that MyApp is called with `const`
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);  // Add const constructor here
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SplashScreen(),
+      home: SplashScreen(),  // Set the home screen to the SplashScreen
     );
   }
 }
@@ -27,11 +82,13 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
+    requestAlarmPermission();  // Request permission when app starts
+
+    // Navigate to the next screen after a 5-second delay
     Timer(Duration(seconds: 5), () {
       Navigator.pushReplacement(
         context,
-        // MaterialPageRoute(builder: (context) => AnalyzePage()),
-        MaterialPageRoute(builder: (context) => Mainsignup()),
+        MaterialPageRoute(builder: (context) => Mainsignup()), // Navigate to SignUp page after 5 seconds
       );
     });
   }
@@ -39,13 +96,15 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF85C1E5), // Set the background color
+      backgroundColor: Color(0xFF85C1E5),  // Set the background color
       body: Center(
-        child: Image.asset('images/fynaura.png'),
+        child: Image.asset('images/fynaura.png'),  // Display the splash screen image
       ),
     );
   }
 }
+
+// Function to request permission to schedule exact alarms (if necessary)
 void requestAlarmPermission() async {
   if (await Permission.scheduleExactAlarm.isDenied) {
     await Permission.scheduleExactAlarm.request();
