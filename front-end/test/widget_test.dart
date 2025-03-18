523efaff2fd11fb46eb07eb572b3f25d86012f86
Dimalsha Perera <139336872+DimalshaPerera@users.noreply.github.com>
2025-03-18 15:36:32 +0530

    SCRUM -011-CI- Fixing issues

--- front-end/test/widget_test.dart
+++ front-end/test/widget_test.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:fynaura/main.dart';  // Import your app's main entry point
+import 'package:fynaura/main.dart';  // Import your app entry point
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build the app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
-    // Verify that the counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Ensure the counter starts at 0.
+    expect(find.text('0'), findsOneWidget);  // Verify that the initial counter is 0
+    expect(find.text('1'), findsNothing);    // Ensure that the counter doesn't start at 1
 
     // Tap the '+' icon and trigger a frame.
     await tester.tap(find.byIcon(Icons.add));  // Tap the '+' icon
@@ -20,4 +20,3 @@ void main() {
     expect(find.text('1'), findsOneWidget);  // 1 should now be visible
   });
 }
-
