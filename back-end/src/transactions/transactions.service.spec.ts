b1e05ec6ad3a394956e143e27df0264d76d09fb1
Dimalsha Perera <139336872+DimalshaPerera@users.noreply.github.com>
2025-03-24 00:17:01 +0530

    scrum -011- test

--- back-end/src/transactions/transactions.service.spec.ts
+++ back-end/src/transactions/transactions.service.spec.ts
@@ -1,13 +1,50 @@
+// /* eslint-disable prettier/prettier */
+// import { Test, TestingModule } from '@nestjs/testing';
+// import { TransactionsService } from './transactions.service';
+
+// describe('TransactionsService', () => {
+//   let service: TransactionsService;
+
+//   beforeEach(async () => {
+//     const module: TestingModule = await Test.createTestingModule({
+//       providers: [TransactionsService],
+//     }).compile();
+
+//     service = module.get<TransactionsService>(TransactionsService);
+//   });
+
+//   it('should be defined', () => {
+//     expect(service).toBeDefined();
+//   });
+// });
 /* eslint-disable prettier/prettier */
 import { Test, TestingModule } from '@nestjs/testing';
 import { TransactionsService } from './transactions.service';
+import { getModelToken } from '@nestjs/mongoose';
 
 describe('TransactionsService', () => {
   let service: TransactionsService;
 
+  // Create a mock for the TransactionModel
+  const mockTransactionModel = {
+    find: jest.fn().mockReturnThis(),
+    findOne: jest.fn().mockReturnThis(),
+    findById: jest.fn().mockReturnThis(),
+    create: jest.fn(),
+    save: jest.fn(),
+    exec: jest.fn(),
+    // Add any other methods your service uses
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TransactionsService],
+      providers: [
+        TransactionsService,
+        {
+          provide: getModelToken('Transaction'),
+          useValue: mockTransactionModel,
+        },
+      ],
     }).compile();
 
     service = module.get<TransactionsService>(TransactionsService);
